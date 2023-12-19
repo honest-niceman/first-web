ea204933c3c06ed2781d7000c88d4e4fa66e1836
Georgii Vlasov <honest.niceman@gmail.com>
2023-12-19 22:14:11 +0400

    Task finished

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,42 @@
 package org.example;
 
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Введи свой вариант ответа: камень, ножницы или бумага. Либо введите 'выход'");
+        //                      0         1         2
+        String[] choices = {"камень", "ножницы", "бумага"};
+        Random random = new Random();
+        int userWins = 0;
+        int computerWins = 0;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (true) {
+            int computerChoiceIdx = random.nextInt(choices.length);
+            String computerChoice = choices[computerChoiceIdx];
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Твой вариант ответа:");
+            String userChoice = scanner.nextLine().toLowerCase(Locale.ROOT).trim();
+
+            if (userChoice.equals("выход")) {
+                break;
+            }
+
+            if (computerChoice.equals(userChoice)) {
+                System.out.println("Ничья!");
+            } else if ((userChoice.equals("камень") && computerChoice.equals("ножницы")) ||
+                    (userChoice.equals("бумага") && computerChoice.equals("камень")) ||
+                    (userChoice.equals("ножницы") && computerChoice.equals("бумага"))) {
+                userWins++;
+                System.out.println("Ты победил! Твой счёт: " + userWins);
+            } else {
+                computerWins++;
+                System.out.println("Компьютер победил! Счёт компьютера: " + computerWins);
+            }
         }
+        System.out.println("Всего хорошего!");
     }
 }
